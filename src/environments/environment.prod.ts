45ab98216fe2743ae964bccbc3950186e31db51e
Antoine Donrault <antoine.donrault@gmail.com>
2020-02-18 00:18:22 +0100

    Autoplay video home page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,8 +6,6 @@ export const environment = {
     databaseURL: "https://duckflix-d1f80.firebaseio.com",
     projectId: "duckflix-d1f80",
     storageBucket: "duckflix-d1f80.appspot.com",
-    messagingSenderId: "821353078645",
-    appId: "1:821353078645:web:8fbb6553b701fb3055bd3c",
-    measurementId: "G-SP5P10KRQ4"
+    messagingSenderId: "821353078645"
   }
 };
